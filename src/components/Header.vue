ce37f676598ab893e8a3c5fa1d8f3c9ab23f5c87
Aya Iida <ayaomi0811@gmail.com>
2023-06-20 20:02:00 -0700

    added component file

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <header>
+        <section>
+            <a href="#">
+                <img src="./img/sneakers.png" alt="logo1">
+                <h2>Street Kicks</h2>
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Sale</a></li>
+                    <li><a href="#">New</a></li>
+                </ul>
+            </nav>
+            <article>
+                <form action="#">
+                    <input type="search">
+                    <i class="fa-light fa-magnifying-glass"></i>
+                </form>
+                <a href="#">
+                    <i class="fa-light fa-heart"></i>
+                </a>
+                <a href="#">
+                    <i class="fa-sharp fa-light fa-cart-shopping"></i>
+                </a>
+                <a href="#">
+                    <i class="fa-light fa-user"></i>
+                </a>
+            </article>
+        </section>
+    </header>
+</template>
+
+<script>
+
+export default{
+    name: 'Header',
+
+}
+</script>
+
+<style>
+
+</style>
