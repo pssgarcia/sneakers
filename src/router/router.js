b619a07e8a29fd13b9be28f796e202a384ec7388
Aya Iida <ayaomi0811@gmail.com>
2023-07-06 13:38:09 -0700

    hello world deleted

--- src/router/router.js
+++ src/router/router.js
@@ -1,14 +1,8 @@
 import AboutPage from './../components/pages/AboutPage.vue';
 import ContactPage from './../components/pages/ContactPage.vue';
-import HomePage from './../components/pages/HomePage.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-    {
-        path: "/",
-        name: "HomePage",
-        component: HomePage
-    },
     {
         path: "/about",
         name: "AboutPage",
